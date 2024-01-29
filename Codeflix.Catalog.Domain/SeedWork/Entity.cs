82d25ec8312b56d3ef7d1f6d3f75cfa5af45cd5b
valves <valves@WKSJUN000112>
2024-01-29 09:53:32 -0300

    tests: add base test

---
+++ Codeflix.Catalog.Domain/SeedWork/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeflix.Catalog.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        protected Entity() => Id = Guid.NewGuid();
+    }
+}
---
+++ Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,25 @@
+﻿using Codeflix.Catalog.Domain.Exceptions;
+
+
+namespace Codeflix.Catalog.Domain.Validation
+{
+    public class DomainValidation
+    {
+        public static void NotNull(object? target, string fieldName) 
+        {
+            if (target is null)
+                throw new EntityValidationException($"{fieldName} should not be null");
+        }
+
+        public static void MinLength(string target, int minLength, string fieldName)
+        {
+            if (target.Length < minLength)
+                throw new EntityValidationException($"{fieldName} should be at least {minLength} characters long");
+        }
+        public static void MaxLength(string target, int maxLength, string fieldName)fu
+        {
+            if (target.Length > maxLength)
+                throw new EntityValidationException($"{fieldName} should be less or equal {maxLength} characters long");
+        }
+    }
+}
